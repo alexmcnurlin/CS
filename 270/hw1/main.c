90c0a5f1649d6a91401ccce80b0481ccf6fdc2b3
alexmcnurlin <alemcnurlin@gmail.com>
2016-09-07 20:13:57 -0700

    270: Fix quicksort

--- 270/hw1/main.c
+++ 270/hw1/main.c
@@ -10,8 +10,8 @@
 //#include <insertion_sort.c>
 
 int main() {
-  int arrToSort[20] = {5, 3, 6, 7, 8, 6, 4, 6, 8, 6, 4, 4, 6, 7, 8, 8, 5, 4, 3, 5};
+  int arrSize = 20;
+  int arrToSort[] = {5, 234, 73, 8, 1, 2345, 345, 76, 23, 454, 3, 2, 3, 5, 5, 78, 354, 2, 2345, 35};
 
-  //bubble_sort(arrToSort, 19);
-  quick_sort(arrToSort, 19, 0, 18);
+  quick_sort(arrToSort, arrSize, 0, arrSize-1);
 }
--- 270/hw1/quick_sort.h
+++ 270/hw1/quick_sort.h
@@ -4,16 +4,24 @@
 // Performs a quicksort on the given array
 // Takes array, array size, and first/last indices as arguments
 // Doesn't modify original array
-int quick_sort(int inArr[], int arrSize, int first, int last) {
+int quick_sort_rec(int inArr[], int arrSize, int first, int last) {
+  //printf("arrSize %i, first %i, last %i\n", arrSize, first, last);
+
+  int newSize = last-first+1;
+  if (newSize < 1) {
+    return 0;
+  } else if (newSize == 1) {
+    printf("%i ", inArr[first]);
+    return 0;
+  }
+
   // Copy contents of inArr to a new array
   int newArr[arrSize];
   for (int i=0; i<arrSize; i++) {
     newArr[i] = inArr[i];
   }
 
-  int pivot = first+(arrSize)/2;
-  //int pivot = newArr[pivot_i];
-
+  int pivot = first+(newSize)/2;
   int i = first;
   int j = last;
   while (i <= pivot) {
@@ -21,14 +29,15 @@ int quick_sort(int inArr[], int arrSize, int first, int last) {
 
       int swapped = 0;
       while (!swapped) {
-
-        if (newArr[j] < newArr[pivot]) {
+        if (newArr[j] <= newArr[pivot]) {
           int temp = newArr[j];
           newArr[j] = newArr[i];
           newArr[i] = temp;
 
           if (i == pivot) {
             pivot = j;
+          } else if (j == pivot) {
+            pivot = i;
           }
 
           swapped = 1;
@@ -44,11 +53,14 @@ int quick_sort(int inArr[], int arrSize, int first, int last) {
     }
   }
 
-  /* size1 = pivot-first; */
-  /* size2 = last-pivot */
-  quick_sort(newArr, pivot-first, first, pivot-1);
+  quick_sort_rec(newArr, arrSize, first, pivot-1);
   printf("%i ", newArr[pivot]);
-  quick_sort(newArr, last-pivot, pivot+1, last);
+  quick_sort_rec(newArr, arrSize, pivot+1, last);
 
   return 0;
+}
+
+int quick_sort(int inArr[], int arrSize, int first, int last) {
+  printf("Quick_sort: \n");
+  quick_sort_rec(inArr, arrSize, first, last);
 }
